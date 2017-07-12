7a853441ea6950bd874eac9e3c55a4886f8a9400
Tom Marsh <t.r.marsh@warwick.ac.uk>
2017-07-12 21:45:39 +0100

    script to plot a light curve based on a model file

--- scripts/lcurve.py
+++ scripts/lcurve.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,14 +8,16 @@ s1     = 10.
 s2     = 1. 
 r1     = 0.025
 r2     = 0.021
-limb1  = seclipse.Limb(0.6)
-limb2  = seclipse.Limb(0.3)
+limb1  = seclipse.Limb(seclipse.Limb.POLY,0.6)
+limb2  = seclipse.Limb(seclipse.Limb.POLY,0.3)
 n1     = 50
 n2     = 50
 iangle = 89.
+b1     = 0.001
+b2     = 0.002
 
 phase = np.linspace(-0.1,1.6,15000)
-flux  = seclipse.circle(phase,iangle,s1,s2,r1,r2,limb1,limb2,n1,n2)
+flux = seclipse.circle(phase,iangle,s1,s2,r1,r2,limb1,limb2,n1,n2,b1,b2)
 
 plt.plot(phase, flux)
 plt.show()
