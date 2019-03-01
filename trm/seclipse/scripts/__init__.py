52a8917f2ae2e63563396f9c4d3d02e7f072a94a
Tom Marsh <t.r.marsh@warwick.ac.uk>
2019-03-01 22:17:55 +0000

    added mcmc to initialising script

--- trm/seclipse/scripts/__init__.py
+++ trm/seclipse/scripts/__init__.py
@@ -3,8 +3,10 @@ Scripts sub-module of seclipse contains the commands to runs models etc
 """
 
 from .lcmodel import lcmodel
+from .mcmc import mcmc
 
 __all__ = [ \
             'lcmodel',
+            'mcmc',
         ]
 
